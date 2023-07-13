aa2500ac61c0b8f7d9b592796a1d6711ca91059e
Justice Otuya <ujustice@quidax.com>
2023-07-13 23:23:39 +0100

    feat add header

--- src/components/header-strip/HeaderStrip.tsx
+++ src/components/header-strip/HeaderStrip.tsx
@@ -1,23 +1,19 @@
 import React from 'react'
+import { fomartCurrency } from '../../util'
+import { useGame } from '../../context/game-context'
 
-type Props = {
-    betAmount: number
-    winAmount: number
-    balance: number
-}
-
-const HeaderStrip = (props: Props) => {
-    const { betAmount, winAmount, balance } = props
+const HeaderStrip = () => {
+    const { betAmount, winAmount, balance } = useGame()
     return (
-        <header className='bg-casino-black-light text-casino-gold flex items-center justify-center gap-20 font-bold py-2'>
-            <p className='flex gap-1 uppercase'>
-                <span>Balance:</span><span className="text-white">{balance}</span>
+        <header className='flex items-center justify-center gap-20 py-2 font-bold bg-casino-black-light text-casino-gold'>
+            <p className='flex gap-1 uppercase' data-testid="balance">
+                <span >Balance:</span><span className="text-white">{fomartCurrency(balance)}</span>
             </p>
-            <p className='flex gap-1 uppercase'>
-                <span>Bet:</span><span className="text-white">{betAmount}</span>
+            <p data-testid="bet" className='flex gap-1 uppercase'>
+                <span>Bet:</span><span className="text-white">{fomartCurrency(betAmount)}</span>
             </p>
-            <p className='flex gap-1 uppercase'>
-                <span>Win:</span><span className="text-white">{winAmount}</span>
+            <p data-testid="winAmount" className='flex gap-1 uppercase'>
+                <span>Win:</span><span className="text-white">{fomartCurrency(winAmount)}</span>
             </p>
         </header>
     )
